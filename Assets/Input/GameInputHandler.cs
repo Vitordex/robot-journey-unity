bed9e19d76787888f831c940ee8bfb1b9ada5b35
vitordex <vitordexjesus@gmail.com>
2020-06-22 14:46:24 -0300

    feat(game): Add turn platforms input and input handler
    Issues: #1

---
+++ Assets/Input/GameInputHandler.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace RobotJourney.Input
+{
+    public class GameInputHandler : MonoBehaviour
+    {
+        private MainActions _actions;
+
+        public UnityEvent turnPlatformsHandler;
+
+        private void Awake()
+        {
+            _actions = new MainActions();
+
+            _actions.Game.TurnPlatforms.performed += (context) => turnPlatformsHandler.Invoke();
+        }
+
+        private void OnEnable()
+        {
+            _actions.Game.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _actions.Game.Disable();
+        }
+    }
+}
