50f04aa2969382ca851822cc491ecc3c96420f4b
vitordex <vitordexjesus@gmail.com>
2020-06-29 07:55:49 -0300

    fix(game): Fix unconsistent physics in movable platform
    Issues: #18

--- Assets/Runtime/Player/PlayerComponent.cs
+++ Assets/Runtime/Player/PlayerComponent.cs
@@ -6,10 +6,21 @@ namespace Runtime.Player
     public class PlayerComponent : MonoBehaviour
     {
         public UnityEvent killedPlayer;
+        private Transform _cachedTransform;
+
+        private void Awake()
+        {
+            _cachedTransform = GetComponent<Transform>();
+        }
 
         public void KillPlayer()
         {
             killedPlayer.Invoke();
+        }
+
+        public void AttachToTransform(Transform parent)
+        {
+            _cachedTransform.SetParent(parent);
         }
     }
 }
